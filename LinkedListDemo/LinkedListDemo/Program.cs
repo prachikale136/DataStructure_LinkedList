b6179d3d81d7c7a4aa137f212847105079569b00
Prachi <prachikale136@gmail.com>
2023-03-10 02:36:08 +0530

    [Prachi] Add: Ability to create Simple LinkedList

---
+++ LinkedListDemo/LinkedListDemo/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace LinkedListDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
+        }
+    }
+}
